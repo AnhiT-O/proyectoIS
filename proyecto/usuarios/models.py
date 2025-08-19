41747dd8cbc51d4de502eb65ffb1446cd3b02e35
losrivarola98 <losrivarola612@fpuna.edu.py>
2025-08-19 12:52:30 -0300

    pruebas unitarias de models

--- proyecto/usuarios/models.py
+++ proyecto/usuarios/models.py
@@ -9,19 +9,16 @@ class Usuario(AbstractUser):
         ('CI', 'Cédula de Identidad'),
     ]
     
-    nombre = models.CharField(max_length=50)
-    apellido = models.CharField(max_length=50)
-    nombre_usuario = models.CharField(max_length=30, unique=True)
-    tipo_cedula = models.CharField(max_length=1, choices=TIPO_CEDULA_CHOICES)
+    tipo_cedula = models.CharField(max_length=3, choices=TIPO_CEDULA_CHOICES)
     cedula_identidad = models.CharField(max_length=8, unique=True)
-    correo_electronico = models.EmailField(unique=True)
-    
-    # Usar nombre_usuario como campo de autenticación
-    USERNAME_FIELD = 'nombre_usuario'
-    REQUIRED_FIELDS = ['correo_electronico', 'nombre', 'apellido', 'tipo_cedula', 'cedula_identidad']
+    email = models.EmailField(unique=True)
+
+    # Usar username como campo de autenticación
+    USERNAME_FIELD = 'username'
+    REQUIRED_FIELDS = ['email', 'first_name', 'last_name', 'tipo_cedula', 'cedula_identidad']
     
     def __str__(self):
-        return f"{self.nombre} {self.apellido} ({self.nombre_usuario})"
+        return f"{self.first_name} {self.last_name} ({self.username})"
     
     class Meta:
         db_table = 'usuarios'
