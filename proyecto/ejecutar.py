4dc7de7472b819fed7b1423a279200f6e051934b
losrivarola98 <losrivarola612@fpuna.edu.py>
2025-09-16 21:43:26 -0300

    se agrega archivos para ejecución directa del proyecto

---
+++ proyecto/ejecutar.py
@@ -0,0 +1,19 @@
+import signal
+import subprocess
+import os
+import sys
+
+os.chdir(os.path.dirname(os.path.abspath(__file__)))
+
+process = None
+def handle_sigint(signum, frame):
+	global process
+	print("\nServidor detenido por el usuario (Ctrl+C). Cerrando...")
+	if process:
+		process.terminate()
+	sys.exit(0)
+
+signal.signal(signal.SIGINT, handle_sigint)
+
+process = subprocess.Popen(["python", "manage.py", "runserver"])
+process.wait()
