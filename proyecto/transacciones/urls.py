cf8d5bf2fcbdea95c451132afb17744162215325
losrivarola98 <losrivarola612@fpuna.edu.py>
2025-09-18 14:34:48 -0300

    Se añade cliente activo y se mejora barra de progreso

--- proyecto/transacciones/urls.py
+++ proyecto/transacciones/urls.py
@@ -5,6 +5,5 @@ app_name = 'transacciones'
 
 urlpatterns = [
     path('comprar/', views.compra_monto_moneda, name='compra_monto_moneda'),
-    path('comprar/pago/', views.compra_medio_pago, name='compra_medio_pago'),
-    path('ajax/precio-moneda/<int:moneda_id>/', views.obtener_precio_moneda, name='compra_precio_moneda'),
+    path('comprar/medio-pago/', views.compra_medio_pago, name='compra_medio_pago')
 ]
